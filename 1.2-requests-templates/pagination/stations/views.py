6637377ae7d77016abb5f1755afc6f0fc91f7975
Ivan Zharikov <izharikov888@gmail.com>
2022-03-20 16:32:44 +0300

    Pagi commit

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,5 +1,16 @@
+import csv
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.core.paginator import Paginator
+
+
+def read_file():
+    data = []
+    with open('./data-398-2018-08-30.csv', 'r', encoding='utf-8', newline='') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data.append({'Name': row['Name'], 'Street': row['Street'], 'District': row['District']})
+    return data
 
 
 def index(request):
@@ -7,11 +18,16 @@ def index(request):
 
 
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
-
+    data = read_file()
+    page_number = int(request.GET.get('page', 1))
+    paginator = Paginator(data, 10)
+    page = paginator.get_page(page_number)
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+       'bus_stations': list(page),
+       'page': page
     }
     return render(request, 'stations/index.html', context)
+
+
+
+
