86ddcd9f98c5aa63c9350d6f3f209eb44aa63b9e
Ivan Zharikov <izharikov888@gmail.com>
2022-03-21 10:16:59 +0300

    Secong commit

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -2,11 +2,12 @@ import csv
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.core.paginator import Paginator
+from django.conf import settings
 
 
 def read_file():
     data = []
-    with open('./data-398-2018-08-30.csv', 'r', encoding='utf-8', newline='') as file:
+    with open(settings.BUS_STATION_CSV, 'r', encoding='utf-8', newline='') as file:
         reader = csv.DictReader(file)
         for row in reader:
             data.append({'Name': row['Name'], 'Street': row['Street'], 'District': row['District']})
