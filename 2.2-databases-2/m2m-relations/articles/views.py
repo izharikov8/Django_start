dbf49078edeeccc825b63e619ea64625a1826753
Ivan Zharikov <izharikov888@gmail.com>
2022-04-05 18:31:09 +0300

    Initial commit

--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -4,11 +4,11 @@ from articles.models import Article
 
 
 def articles_list(request):
+    object_list = Article.objects.all()
     template = 'articles/news.html'
-    context = {}
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+    context = {
+        'object_list': object_list
+    }
     ordering = '-published_at'
 
     return render(request, template, context)
